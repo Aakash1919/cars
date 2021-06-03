69bb0d165ecc2cfd99a3203018939c7d93f60949
Aakash1919 <programmer.aakash@gmail.com>
2021-06-03 13:45:10 +0530

    changed fillable fields

--- project/app/Models/Payment.php
+++ project/app/Models/Payment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    protected $fillable = ['user_id', 'plan_id', 'car_id', 'status'];
+    protected $fillable = ['user_id', 'plan_id', 'car_id', 'status', 'amount'];
 
     public function user() {
       return $this->belongsTo('App\Models\User');
