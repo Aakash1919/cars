6392e01e478caf832e8d3905df9074a0ae18d421
Aakash1919 <programmer.aakash@gmail.com>
2021-06-25 01:56:46 +0530

    All changes done as asked by client on buyer seller page

--- project/resources/views/front/buyerseller.blade.php
+++ project/resources/views/front/buyerseller.blade.php
@@ -65,7 +65,7 @@
     <div class="container">
         <div class="user-benefits">
             <article class="section-heading">
-                <h4>Benefits of</h4>
+                {{-- <h4>Benefits of</h4> --}}
                 <h2 class="title">Becoming a Buyer/Seller</h2>
             </article>
             <div class="row">
@@ -172,6 +172,116 @@
     </div>
 </section>
 
+
+<section class="bg-gray custom-section">
+    <div class="container">
+        <div class="user-benefits">
+            <article class="section-heading">
+                {{-- <h4>Benefits of</h4> --}}
+                <h2 class="title">Becoming a PREMIUM MEMBER </h2>
+            </article>
+            <div class="row">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="media">
+                        <div class="step-circle">
+                            <figure>
+                                <img class="cars" src="{{asset('assets/front/images/bid.png')}}" alt="">
+                                <div class="step-count">
+                                    1
+                                </div>
+                            </figure>
+                        </div>
+                        <div class="media-body ml-5">
+                            <h5 class="mt-0">Bid For Cars</h5>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="media">
+                        <div class="step-circle">
+                            <figure>
+                                <img class="cars" src="{{asset('assets/front/images/no-commision-icon.png')}}" alt="">
+                                <div class="step-count">
+                                    2
+                                </div>
+                            </figure>
+                        </div>
+                        <div class="media-body ml-5">
+                            <h5 class="mt-0">No Commission</h5>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="media">
+                        <div class="step-circle">
+                            <figure>
+                                <img class="cars" src="{{asset('assets/front/images/private-icon.png')}}" alt="">
+                                <div class="step-count">
+                                    3
+                                </div>
+                            </figure>
+                        </div>                   
+                        <div class="media-body ml-5">
+                            <h5 class="mt-0">Private Bids</h5>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="media">
+                        <div class="step-circle">
+                            <figure>
+                                <img class="cars" src="{{asset('assets/front/images/discount.png')}}" alt="">
+                                <div class="step-count">
+                                    4
+                                </div>
+                            </figure>
+                        </div>
+                        <div class="media-body ml-5">
+                            <h5 class="mt-0">Discounted Listing</h5>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="custom-section user-get-started">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-xl-5 col-lg-6">
+                <div class="elegant-pricing-tables style-2 text-center px-4 pb-5 custom-pricing">
+                    <div class="pricing-head">
+                        <span class="price">
+                            <sup>$ </sup>
+                            <span class="price-digit">99</span><br>
+                            <span class="price-month">/ week</span>
+                        </span>
+                    </div>
+                    <div class="pricing-detail">
+                        <ul class="list">
+                            <li>Advanced  Vehicle Information <i class="fa fa-check"></i></li>
+                            <li>Unlimited <span class="badge custom-badge">Free Listing</span></li>
+                            <li>Full Vehicle Information <i class="fa fa-check"></i></li>
+                            <li>Bid On Vehicles <i class="fa fa-check"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-7 col-lg-6 pl-xl-5 mt-sm-4">
+                <article class="text-white">
+                    <h2 class="custom-title">Get Started as a Premium Member</h2>
+                    <a href="#" class="btn btn-custom btn-md">Start Buying & Selling</a>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 @endsection
